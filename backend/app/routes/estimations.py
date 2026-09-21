3dd4528c7828316c9ad5481577275e3819864e29
[Valeriia <[valeriiakovalovakotya@gmail.com]>
2026-09-21 22:34:15 +0200

    added admin endpoint to do not wait for monday to run fill in skripts

--- backend/app/routes/estimations.py
+++ backend/app/routes/estimations.py
@@ -46,7 +46,7 @@ class EstimationResponse(BaseModel):
 
 from fastapi import BackgroundTasks
 
-@app.post("/admin/sync-locations", tags=["Admin"])
+@router.post("/admin/sync-locations", tags=["Admin"])
 async def trigger_scraper(background_tasks: BackgroundTasks):
     from app.scraper.run_all import main as run_scraper_job
     background_tasks.add_task(run_scraper_job)
